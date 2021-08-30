94c9ddf6f86210e9ee75ffef7b6c480e06fb7a05
Muhammed <muhammadburhan964@gmail.com>
2021-08-30 16:49:43 +0300

    commit tree 175c802454a60e61cd6b97d8b4a2f1af1d73a350

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,21 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCats } from "./catsSlice";
+import CatList from "./CatList";
 function Cats() {
+  const catPics = useSelector((state) => state.entities);
+  const status = useSelector((state) => state.status);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      {/*status === "idle" ? <CatList catPics={catPics} /> : <h3>loading...</h3>*/}
+      <CatList catPics={catPics} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import catReducer from "./features/cats/catsSlice";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
+import "bootstrap/dist/css/bootstrap.min.css";
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const Store = createStore(catReducer, composedEnhancer);
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
